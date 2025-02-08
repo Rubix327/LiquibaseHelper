e8ce895af454f662c962110c49b06d5a5d959232
Rubix327 <yarik.cool1@mail.ru>
2025-02-08 17:51:33 +0300

    Генератор метаданных

---
+++ src/main/java/me/rubix327/liquibasehelper/form/FormUtils.java
@@ -0,0 +1,9 @@
+package me.rubix327.liquibasehelper.form;
+
+import javax.swing.*;
+
+public class FormUtils {
+
+    public static final ImageIcon PLUGIN_ICON = new ImageIcon("D:/IdeaProjects/LiquibaseNavigator/src/main/resources/img/liquibase.svg");
+
+}
